4556d2ca11b201f722506004a79adcdbf36d02fd
Toby Law <toby@tzcl.me>
2022-03-02 20:59:21 +1100

    Fixed duplicate logic

--- wordle.cpp
+++ wordle.cpp
@@ -1,8 +1,13 @@
 #include <algorithm>
+#include <cassert>
+#include <deque>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <string_view>
+#include <unordered_map>
+
+using word_map = std::unordered_map<char, std::deque<int>>;
 
 std::string get_guess() {
   std::string guess; // TODO: validate 5 letters (and need to check dictionary)
@@ -17,28 +22,39 @@ std::string get_guess() {
 // . -- wrong letter
 // Y -- right letter wrong spot
 // G -- right letter right spot
-std::string process_guess(const std::string &guess, const std::string &word) {
-  // Naive algorithm: loop over the word and colour first instance of each
-  // letter in guess
+std::string process_guess(const std::string &guess, word_map letters) {
   std::string response;
-  std::string append;
-
   for (int i = 0; i < guess.size(); ++i) {
-    append = ".";
-    for (int j = 0; j < word.size(); ++j) {
-      if (guess[i] == word[j]) {
-        i == j ? append = "G" : append = "Y";
-        break;
-      }
+    if (letters.count(guess[i])) {
+      if (letters[guess[i]].front() == i)
+        response += "G";
+      else
+        response += "Y";
+      letters[guess[i]].pop_front();
+    } else {
+      response += ".";
     }
-    response += append;
   }
 
   return response;
 }
 
 int main() {
-  const std::string word = "NASTY";
+  const word_map NASTY{{'N', std::deque<int>{0}},
+                       {'A', std::deque<int>{1}},
+                       {'S', std::deque<int>{2}},
+                       {'T', std::deque<int>{3}},
+                       {'Y', std::deque<int>{4}}};
+
+  const word_map TASTY{{'T', std::deque<int>{0, 3}},
+                       {'A', std::deque<int>{1}},
+                       {'S', std::deque<int>{2}},
+                       {'Y', std::deque<int>{4}}};
+
+  assert(process_guess("FEAST", NASTY) == "..YYY");
+  assert(process_guess("TREAT", TASTY) == "G..YY");
+
+  const std::string word = "TASTY";
   int guesses = 6;
 
   while (guesses > 0) {
@@ -49,7 +65,7 @@ int main() {
       guess = get_guess();
     }
     // Respond to guess
-    std::string response = process_guess(guess, word);
+    std::string response = process_guess(guess, TASTY);
     if (response == "GGGGG") {
       std::stringstream result;
       result << "You won in ";
