0450cfef81a87fd7654e1e9ffed18fecb679c104
Toby Law <toby@tzcl.me>
2022-03-03 08:36:36 +1100

    Set up clojure for discovery

---
+++ analysis.clj
@@ -0,0 +1,13 @@
+(->> (slurp "answers")
+     (clojure.string/split-lines)
+     (count))
+
+(->> (slurp "words")
+     (clojure.string/split-lines)
+     (count))
+
+(+ 2309 10638)
+
+(def words (slurp "answers"))
+(+ 2 2)
+
