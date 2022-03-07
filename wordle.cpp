48999b874ed3662b5f5f1c43de3db2370eddf719
Toby Law <toby@tzcl.me>
2022-03-07 22:13:54 +1100

    Changed starting word to be random

--- wordle.cpp
+++ wordle.cpp
@@ -1,11 +1,13 @@
 #include <algorithm>
 #include <cassert>
 #include <deque>
+#include <fstream>
 #include <iostream>
+#include <random>
 #include <sstream>
 #include <string>
-#include <string_view>
 #include <unordered_set>
+#include <vector>
 
 std::string get_guess() {
   std::string guess; // TODO: validate 5 letters (and need to check dictionary)
@@ -43,23 +45,51 @@ std::string process_guess(const std::string &guess, const std::string &word) {
   return response;
 }
 
+std::vector<std::string> read_words(std::string filename) {
+  std::vector<std::string> result;
+  std::ifstream fs(filename);
+  std::string word;
+
+  while (std::getline(fs, word)) {
+    result.push_back(word);
+  }
+
+  return result;
+}
+
 int main() {
+  // Test cases
   assert(process_guess("FEAST", "NASTY") == "XXYYY");
   assert(process_guess("TREAT", "TASTY") == "GXXYY");
   assert(process_guess("NASTY", "TASTY") == "XGGGG");
 
-  const std::string word = "TASTY";
+  // Read in valid answers and guesses
+  const std::vector<std::string> ANSWERS = read_words("answers");
+  const std::vector<std::string> GUESSES = read_words("guesses");
+
+  assert(ANSWERS.size() == 2309);
+  assert(GUESSES.size() == 10638);
+
+  std::random_device rd;
+  std::mt19937 mt(rd());
+
+  std::uniform_int_distribution<int> rand(0, ANSWERS.size() - 1);
+  std::string word = ANSWERS[rand(mt)];
+  std::transform(word.begin(), word.end(), word.begin(), ::toupper);
+  std::cout << "The word is " << word << std::endl;
+
   int guesses = 6;
 
   while (guesses > 0) {
     // Get guess
     std::string guess = get_guess();
-    while (guess.length() != 5) {
-      std::cout << "Guess was the wrong length!" << std::endl;
+    while (guess.length() != 5 ||
+           std::find(GUESSES.begin(), GUESSES.end(), guess) != GUESSES.end()) {
+      std::cout << "Invalid guess!" << std::endl;
       guess = get_guess();
     }
     // Respond to guess
-    std::string response = process_guess(guess, "TASTY");
+    std::string response = process_guess(guess, word);
     if (response == "GGGGG") {
       std::stringstream result;
       result << "You won in ";
